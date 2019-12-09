5e4e1983e4f293adb901be49769a9fb6577da07a
Allison Johnson <allison.cuttler@gmail.com>
2019-12-09 19:32:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   len = array.size 
-  if len == 2 
+  if len == 1 
+    str = array[0]
+  elsif len == 2 
     str = array.join(" and ")
   else
     array[len - 1] = "and #{array[len - 1]}"
